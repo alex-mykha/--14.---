e3466ceee3194051c99ad8ef40d8acb1162d43f0
Alex Mykha <mykha92@gmail.com>
2024-05-07 20:27:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const oneNum = +prompt("Ведите первое число")
+const twoNum = +prompt("Ведите Второе число")
+const threNum = +prompt("Ведите Третие число")
+
+const result = (oneNum + twoNum + threNum)/3
+alert (`${result.toFixed(2)}`)
